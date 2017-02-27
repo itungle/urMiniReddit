d3b86f60e207c8af230dac72a2d709e41bd960a4
Tung Le <itungle@gmail.com>
2017-02-26 23:21:42 -0800

    Added initializer to display previous saved state. Added display the subreddit bar. Added feature to save your subreddit into chrome.storage.sync.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
+var URL = "http://www.reddit.com/r/";
+var DOTJSON = ".json";
+
 function init() {
     "use strict";
+    var subredditList;
     chrome.storage.sync.get("subreddits", function(data) {
-       if (typeof data.subreddits == "undefined") {
-           console.log("nothing in storage");         
-       } else {
-           console.log("something in storage");
-       }
-       console.log(data.subreddits);
+        if (typeof data.subreddits == "undefined") {
+            subredditList = {
+                "popular": URL + "popular" + DOTJSON,
+                "random": URL + "random" + DOTJSON,
+                "all": URL + "all" + DOTJSON
+            };
+            chrome.storage.sync.set({ "subreddits": subredditList }, function() {
+                displaySubredditBar(subredditList);
+            });
+        } else {
+            subredditList = data.subreddits;
+            console.log(subredditList);
+            displaySubredditBar(data.subreddits);
+        }
     });
 };
 
+function displaySubredditBar(subreddits) {
+    var nameBar = $("#subreddits-bar");
+    $(nameBar).empty();
+    for (var name in subreddits) {
+        $(nameBar).append("<a class='anchor-margin'>" + name + "</a>");
+    }
+}
 
-$(document).ready(function() {
-    console.log("ready");
+var main = function() {
+    //chrome.storage.sync.clear();
     init();
-    
-    $("#addNewSubredditButton").on("click", function() {
-       var subreddit = $("#subredditName").val();
-       $("#subredits").append("<a>" + subreddit + "</a>");
-    });
-});
+}
+
+var addNewSubreddit = function() {
+    var newName = $("#subredditName").val();
+    console.log(newName);
+    chrome.storage.sync.get("subreddits", function(data) {
+        var subreddits = data.subreddits;
+        subreddits[newName] = URL + newName + DOTJSON;
+        chrome.storage.sync.set({ "subreddits": subreddits }, function() {
+            console.log("success");
+            displaySubredditBar(subreddits);
+            console.log(subreddits);
+        })
+    })
+}
+
+document.getElementById("addNewSubredditButton").addEventListener("click", addNewSubreddit);
+document.addEventListener("DOMContentLoaded", main);
