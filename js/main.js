e9dedf50337c14a1acf6a14e7800d617a383f20e
Tung Le <itungle@gmail.com>
2017-02-28 22:29:28 -0800

    Added bootstrap libs so icons would work. Added template func. Display remove and add btn

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ var DOTJSON = ".json";
 
 function init() {
     "use strict";
+    $("#error-message").html("");
+    console.log(document.getElementsByClassName("remove-button"));
+
     var subredditList;
     chrome.storage.sync.get("subreddits", function(data) {
         if (typeof data.subreddits == "undefined") {
             subredditList = {
+                "all": URL + "all" + DOTJSON,
                 "popular": URL + "popular" + DOTJSON,
-                "random": URL + "random" + DOTJSON,
-                "all": URL + "all" + DOTJSON
+                "random": URL + "random" + DOTJSON
             };
             chrome.storage.sync.set({ "subreddits": subredditList }, function() {
                 displaySubredditBar(subredditList);
@@ -22,28 +25,47 @@ function init() {
     });
 };
 
+function newSubredditTemplate(name) {
+    var openDiv = "<div class='row'>";
+    var removeBtnCol = "<div class='col-xs-2'>";
+    var removeBtn = "<button type='button' class='btn btn-default btn-sm btn-danger remove-button' id='" + name + "'><span class='glyphicon glyphicon-minus-sign'></span></button>";
+    var anchorCol = "<div class='col-xs-10'>";
+    var anchorBtn = "<a class='btn btn-default btn-name-width ' role='button'>" + name + "</a>";
+    var closeDiv = "</div>";
+    var template = openDiv + removeBtnCol + removeBtn + closeDiv + anchorCol + anchorBtn + closeDiv + closeDiv;
+
+    return template;
+}
+
+
 function displaySubredditBar(subreddits) {
-    var nameBar = $("#item-list");
+    var nameBar = $("#menu");
     $(nameBar).empty();
     for (var name in subreddits) {
-        $(nameBar).append("<div><a class='anchor-margin'>" + name + "</a></div>");
+        var template = newSubredditTemplate(name);
+        $(nameBar).append(template);
     }
 }
 
+var removeSubreddit = function() {
+    console.log(this);
+}
+
 var addNewSubreddit = function() {
-    var newName = $("#subredditName").val();
-    console.log(newName);
+    var newName = $("#subreddit-name").val();
+    $("#error-message").html("");
     chrome.storage.sync.get("subreddits", function(data) {
         var subreddits = data.subreddits;
-        if (subreddits[newName] === "undefined") {
+
+        if (!(newName in subreddits)) {
             subreddits[newName] = URL + newName + DOTJSON;
             chrome.storage.sync.set({ "subreddits": subreddits }, function() {
-                console.log("success");
+                console.log("successfully added new subreddit");
                 displaySubredditBar(subreddits);
-                console.log(subreddits);
             })
         } else {
-            var errorMsg = "Subreddit existed in your list";
+            var errorMsg = "Subreddit already existed";
+            $("#error-message").html(errorMsg).css('color', 'red');
         }
     })
 }
@@ -53,5 +75,8 @@ var main = function() {
     //chrome.storage.sync.clear();
     init();
 }
-document.getElementById("addNewSubredditButton").addEventListener("click", addNewSubreddit);
+
+
+//.addEventListener("click", removeSubreddit);
+document.getElementById("add-new-subreddit-btn").addEventListener("click", addNewSubreddit);
 document.addEventListener("DOMContentLoaded", main);
